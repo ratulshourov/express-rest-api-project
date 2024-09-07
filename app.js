c8fd44779347b8f37d3e3bee3ad2972aae996ab3
ratulshourov <ratulshourov01@gmail.com>
2024-09-08 01:46:28 +0600

    undefined route handle

--- app.js
+++ app.js
@@ -5,4 +5,12 @@ const app=new express();
 
 app.use("/api/v1",router);
 
+//undefined route
+
+app.use("*",(req,res)=>{
+res.status(404).json({
+    'data':'undefined route'
+});
+});
+
 module.exports=app;
